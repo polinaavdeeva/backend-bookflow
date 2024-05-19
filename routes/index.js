95939e4f4680da7656bb92ad682f768ad85d6e55
FarJoke <olegdubovoyy@gmail.com>
2024-05-19 21:41:24 +0300

    Fix: роуты для books

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,31 @@
 const router = require("express").Router();
 const usersRouter = require("./user");
-const booksRouter = require("./book");
 const { auth } = require("../middlewares/auth");
 const NotFoundError = require("../errors/NotFoundError");
 const { createUser } = require("../controllers/signUp");
 const { login } = require("../controllers/signIn");
+const { getBooks, createBook, deleteBook, searchBooks } = require("../controllers/book");
 const {
   validateUserAuthentication,
   validateUserInfo,
 } = require("../middlewares/userValidation");
 
+const {
+  validateDeleteBook,
+  validateBook,
+} = require("../middlewares/bookValidation");
+
+
 router.post("/signup", createUser, validateUserInfo);
 router.post("/signin", login, validateUserAuthentication);
+router.get("/books", getBooks);
+router.get("/books/search", searchBooks);
 
 router.use(auth);
 
 router.use("/users", usersRouter);
-router.use("/books", booksRouter);
+router.post("/books", validateBook, createBook);
+router.delete("books/:bookId", validateDeleteBook, deleteBook);
 
 router.use("*", () => {
   throw new NotFoundError("Ресурс не найден.");
